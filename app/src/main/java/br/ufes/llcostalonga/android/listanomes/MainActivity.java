44d8fb39df0723033d6f43a880ea1d328fb907ef
Leandro Lesqueves Costalonga <llcostalonga@gmail.com>
2017-05-18 15:11:06 -0300

    Initial commit

---
+++ app/src/main/java/br/ufes/llcostalonga/android/listanomes/MainActivity.java
@@ -0,0 +1,13 @@
+package br.ufes.llcostalonga.android.listanomes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
